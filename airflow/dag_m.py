3fe50f9c189cc221211481e9afae0e10640e696e
Victor Morais <victor_morais@yahoo.com.br>
2025-11-11 11:32:25 -0300

    Primeiro Commit

---
+++ airflow/dag_m.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+from airflow.sdk import dag, task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+default_args = {
+  'owner': 'airflow',
+  'depends_on_past': False, 
+  'start_date': datetime(2025,1,1), 
+  'email_on_failure': False, #email em caso de falha
+  'email_on_retry': False, #email em caso de nova tentativa
+  'retries': 0, #no teste vou colocar zero, mas em produção colocaria um número
+  'retry_delay': timedelta(minutes=5)
+}
+
+@dag(
+  dag_id='postgres_to_snowflake', #nome da dag no airflow
+  default_args=default_args,
+  description='Carrega dados de forma incremental do postgres para o Snowflake',
+  schedule=timedelta(days=1),
+  catchup=False, #ele não vai executar para os agendamentos anteriores ao agendamento atual
+  max_active_runs=1 #maximo de execuções ativas simultaneamente
+)#parametros de execução a nivel de branch
+def postgres_to_snowflake_etl():
+  table_names = ['veiculos','estados','cidades','concessionarias', 'vendedores', 'clientes', 'vendas']
+
+  for table_name in table_names:
+    @task(task_id = f'get_max_id_{table_name}')
+    def get_max_primary_ket(table_name:str):
+      with SnowflakeHook(snowflake_conn_id='snowflake').get_conn() as conn:
+        with conn.cursor() as cursor:
+          cursor.execute(f'select MAX(ID_{table_name}) from {table_name}')
+          max_id = cursor.fetchone()[0]
+          return max_id if max_id is not None else 0
+        
+    @task(task_id = f'load_data_{table_name}')
+    def load_incremental_data(table_name:str, max_id:int):
+
+      with PostgresHook(postgress_conn_id='postgres').get_conn() as pg_conn:
+        with pg_conn.cursor() as pg_cursor:
+          primary_key = f'ID_{table_name}'
+
+          pg_cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}'")
+          columns = [row[0] for row in pg_cursor.fetchall()]
+          columns_list_str = ', '.join(columns)
+          placeholders = ', '.join(['%s'] * len(columns))
+          print(placeholders)
+
+          pg_cursor.execute(f'select {columns_list_str} from {table_name} where {primary_key} > {max_id}')
+          rows = pg_cursor.fetchall()
+
+          with SnowflakeHook(snowflake_conn_id = 'snowflake').get_conn() as sf_conn:
+            with sf_conn.cursor() as sf_cursor:
+              innsert_query = f"insert into {table_name} ({columns_list_str}) values ({placeholders})"
+              for row in rows:
+                sf_cursor.execute(innsert_query, row)
+
+    max_id = get_max_primary_ket(table_name)
+    load_incremental_data(table_name, max_id)
+
+postgres_to_snowflake_etl_dag = postgres_to_snowflake_etl()
